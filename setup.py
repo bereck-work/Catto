1ccff1342672c59951260a72b5b8ca08d2685818
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-03-07 02:45:17 +0530

    Version update.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from glob import glob
-from os.path import splitext, basename
+from os.path import basename, splitext
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="catto",
-    version="1.0.0",
+    version="1.0.1",
     description="A simple command line tool that downloads cute animals images of your choice.",
     packages=find_packages("src"),
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
@@ -29,8 +29,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -40,7 +38,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "catto=catto:main_command_interface",
+            "catto=src.catto:main_command_interface",
         ],
     },
 )
