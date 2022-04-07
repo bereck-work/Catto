66bdf683357635712cb8ef5f4194ca242caab38c
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-04-08 02:59:30 +0530

    New version, changes, although i have no idea if this works or not.

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
+import sys
 from glob import glob
 from os.path import basename, splitext
+from pathlib import Path
 
 from setuptools import find_packages, setup
 
+CURRENT_DIR = Path(__file__).parent
+sys.path.insert(0, str(CURRENT_DIR))  # for setuptools.build_meta
+
+
 setup(
     name="catto",
-    version="1.0.1",
+    version="1.0.2",
     description="A simple command line tool that downloads cute animals images of your choice.",
-    packages=find_packages("src"),
-    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
-    package_dir={"": "src"},
+    author="KortaPo",
+    author_email="bereckobrian",
+    packages=find_packages("catto"),
+    py_modules=[splitext(basename(path))[0] for path in glob("catto/*.py")],
+    package_dir={"": "catto"},
     include_package_data=True,
     python_requires=">=3.8",
     install_requires=[
@@ -36,9 +44,5 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Quality Assurance",
     ],
-    entry_points={
-        "console_scripts": [
-            "catto=src.catto:main_command_interface",
-        ],
-    },
+    entry_points={"console_scripts": ["catto = catto:main_command_interface"]},
 )
