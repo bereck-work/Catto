3d4c3c2b462a231bfa7195c77a6bb6206efbc5d5
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-06-07 01:00:35 +0530

    Removed hacky unaliased enums, used dataclasses to represent the json keys, removed the KeyResponseEnum and created a simple enum that stores the specific keys of each api endpoint's response.

--- catto/utils/enums.py
+++ catto/utils/enums.py
@@ -1,58 +1,10 @@
+from dataclasses import dataclass
 from enum import Enum
 
-__all__ = (
-    "AnimalAPIEndpointEnum",
-    "KeyResponseImageEnum",
-    "KeyResponseFactEnum",
-)
+__all__ = ("AnimalAPIEndpointEnum", "AnimalResponseEnum", "ColorEnum")
 
 
-class EnumWithNoAliasing(Enum):
-    """
-    This class subclasses the :class:`Enum` class and overrides the __getattribute__ method to avoid
-    the deduplication check. This is a workaround for labels that get aliased if the values are the same.
-    Taken from https://stackoverflow.com/a/58273746 pretty cool.
-    """
-
-    def __init_subclass__(cls, *kargs, **kwargs):
-        import inspect
-
-        # unfortunately, there's no cleaner ways to retrieve original members
-        for stack in reversed(inspect.stack()):
-            frame_locals = stack[0].f_locals
-            enum_members = frame_locals.get("enum_members")
-            if enum_members is None:
-                try:
-                    enum_members = frame_locals["classdict"]._member_names
-                except (KeyError, AttributeError):
-                    continue
-            break
-        else:
-            raise RuntimeError("Unable to checkout EnumWithNoAliasing members!")
-
-        # patch subclass __getattribute__ to evade deduplication checks
-        cls._shield_members = list(enum_members)
-        cls._shield_getattr = cls.__getattribute__
-
-        def patch(self, key):
-            if key.startswith("_shield_"):
-                return object.__getattribute__(self, key)
-            if key.startswith("_value"):
-                if not hasattr(self, "_name_"):
-                    self._shield_counter = 0
-                elif self._shield_counter < self._shield_members.index(self._name_):
-                    self._shield_counter += 1
-
-                    class unequal:
-                        pass
-
-                    return unequal
-            return self._shield_getattr(key)
-
-        cls.__getattribute__ = patch
-
-
-class AnimalAPIEndpointEnum(EnumWithNoAliasing):
+class AnimalAPIEndpointEnum(Enum):
     """
     This :class:`Enum` stores animal API enpoint urls based on the animal category.
     """
@@ -65,7 +17,7 @@ class AnimalAPIEndpointEnum(EnumWithNoAliasing):
     redpandas = "https://some-random-api.ml/animal/red_panda"
     kangaroo = "https://some-random-api.ml/animal/kangaroo"
     koala = "https://some-random-api.ml/animal/koala"
-    racoon = "https://some-random-api.ml/animal/racoon"
+    raccoon = "https://some-random-api.ml/animal/raccoon"
 
     def __str__(self):
         return self.value
@@ -74,24 +26,36 @@ class AnimalAPIEndpointEnum(EnumWithNoAliasing):
         return f"{self.__class__.__name__}.{self.name}"
 
 
-class KeyResponseImageEnum(EnumWithNoAliasing):
+@dataclass
+class ResponseInterface:
     """
-    This :class:`Enum` stores the API response keys which contain the image url for the specified animal API endpoint,
-    as reffered in the :class:`AnimalAPIEndpoint` Enum.
+    This :class:`dataclass` stores the keys in the json response for each animal category. This is mainly used to
+    get the image url and the fact from the json response.
+    """
+
+    endpoint: AnimalAPIEndpointEnum
+    key_that_contains_image_url: str = None
+    key_that_contains_fact: str = None
+
+
+class AnimalResponseEnum(Enum):
+    """
+    This :class:`Enum` stores the API response keys in :class:`ResponseInterface` which contain the image url and the fact
+    for the specified animal API endpoint, as reffered in the :class:`AnimalAPIEndpoint` Enum.
     This enum basically maps the keys in the JSON response that contains the image URL for each animal API endpoint.
     """
 
     # There would be a better way to do this, but this way if the API response changes, I can just change the enum
     # value for the specific endpoint.
-    dogs = "image"
-    cats = "image"
-    birds = "image"
-    foxes = "image"
-    pandas = "image"
-    redpandas = "image"
-    kangaroo = "image"
-    racoon = "image"
-    koala = "image"
+    dogs = ResponseInterface(AnimalAPIEndpointEnum.dogs, "image", "fact")
+    cats = ResponseInterface(AnimalAPIEndpointEnum.cats, "image", "fact")
+    birds = ResponseInterface(AnimalAPIEndpointEnum.birds, "image", "fact")
+    foxes = ResponseInterface(AnimalAPIEndpointEnum.foxes, "image", "fact")
+    pandas = ResponseInterface(AnimalAPIEndpointEnum.pandas, "image", "fact")
+    redpandas = ResponseInterface(AnimalAPIEndpointEnum.redpandas, "image", "fact")
+    kangaroo = ResponseInterface(AnimalAPIEndpointEnum.kangaroo, "image", "fact")
+    raccoon = ResponseInterface(AnimalAPIEndpointEnum.raccoon, "image", "fact")
+    koala = ResponseInterface(AnimalAPIEndpointEnum.koala, "image", "fact")
 
     def __str__(self):
         return self.value
@@ -100,21 +64,21 @@ class KeyResponseImageEnum(EnumWithNoAliasing):
         return f"{self.__class__.__name__}.{self.name}"
 
 
-class KeyResponseFactEnum(EnumWithNoAliasing):
+class ColorEnum(Enum):
     """
-    This :class:`Enum` stores the API response keys which contain the `fact` for the specified animal API endpoint.
-    This enum basically maps the keys in the JSON response that contains the `fact` for each animal API endpoint.
+    This class subclasses the :class:`Enum` class and stores the color names.
     """
 
-    dogs = "fact"
-    cats = "fact"
-    birds = "fact"
-    foxes = "fact"
-    pandas = "fact"
-    redpandas = "fact"
-    kangaroo = "fact"
-    racoon = "fact"
-    koala = "fact"
+    black = "black"
+    red = "red"
+    green = "green"
+    yellow = "yellow"
+    blue = "blue"
+    magenta = "magenta"
+    cyan = "cyan"
+    white = "white"
+    gray = "gray"
+    grey = "gray"
 
     def __str__(self):
         return self.value
