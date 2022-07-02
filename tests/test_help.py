d308d4cfe1886a4b2e10a5a7241173daa9181e19
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-07-03 01:03:21 +0530

    Updated imports

--- tests/test_help.py
+++ tests/test_help.py
@@ -1,6 +1,6 @@
 from typer.testing import CliRunner
 
-from catto import app
+from src.catto import app
 
 runner = CliRunner()
 
@@ -8,4 +8,4 @@ runner = CliRunner()
 def test_app():
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0
-    assert "Show this message and exit." in result.stdout.strip(" ").strip("\n")
+    assert "show this message and exit." in result.stdout.strip(" ").strip("\n").lower()
