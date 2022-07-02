b1e2cd82bf5d910b407e474bc1a2022172b09346
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-07-03 01:03:00 +0530

    Updated imports

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,6 +1,6 @@
 from typer.testing import CliRunner
 
-from catto import app
+from src.catto import app
 
 runner = CliRunner()
 
@@ -8,4 +8,4 @@ runner = CliRunner()
 def test_app():
     result = runner.invoke(app, ["download", "--category", "cats", "--amount", 1, "--path", "./tests/test_gallery"])
     assert result.exit_code == 0
-    assert "Downloaded '1' images of 'cats' in 'test_gallery'" in result.stdout.strip(" ").strip("\n")
+    assert "downloaded '1' images of 'cats' in 'test_gallery'" in result.stdout.strip(" ").strip("\n").lower()
