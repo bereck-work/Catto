867b9909771b5e146c6c43ba72526f40bdc530b5
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-12-18 00:01:05 +0530

    Added a new exception DataFetchFailed.

--- src/catto/utils/exceptions.py
+++ src/catto/utils/exceptions.py
@@ -1,12 +1,23 @@
+# -*- coding: utf-8 -*-z
+
+__all__ = (
+    "PathNotFound",
+    "InvalidImageURL",
+    "CategoryFactNotFound",
+    "DataFetchFailed",
+    "ImageDownloadFailed",
+)
+
+
 class PathNotFound(Exception):
     """
-    This exception is raised when a path is not found.
+    This exception is raised when a path is not found or does not exist.
     """
 
     pass
 
 
-class InvalidImage(Exception):
+class InvalidImageURL(Exception):
     """
     This exception is raised when the URL provided is not an image.
     """
@@ -14,9 +25,30 @@ class InvalidImage(Exception):
     pass
 
 
-class AnimalFactNotFound(Exception):
+class CategoryFactNotFound(Exception):
     """
     This exception is raised when an animal fact related to the specific animal is not provided.
     """
 
     pass
+
+
+class ImageDownloadFailed(Exception):
+    """
+    This exception is raised when an image has failed to download.
+    """
+
+    def __init__(self, error: str, /, image, reason: str):
+        self.image = image
+        self.reason = reason
+
+
+class DataFetchFailed(Exception):
+    """
+    This exception is raised when the API request fails return the expected data.
+    """
+
+    def __init__(self, error: str, /, status_code: int, reason: str, url: str):
+        self.status_code = status_code
+        self.reason = reason
+        self.url = url
