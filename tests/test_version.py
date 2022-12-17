3eaa475da902667b192d1d1a90c72a5578e2496c
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-12-18 00:08:22 +0530

    Added proper testing to test the version command

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,11 +1,21 @@
-from typer.testing import CliRunner
+# -*- coding: utf-8 -*-
 
-from src.catto import app
+import re
+
+from typer.testing import CliRunner
+import typer
+
+from src.catto import app, __version__
+from src.catto.utils import ColorEnum
 
 runner = CliRunner()
 
 
-def test_app():
+def test_version_command():
     result = runner.invoke(app, ["version"])
+    output = re.compile(
+        rf"(\x1B[@-Z\\-_]|[\x80-\x9A\x9C-\x9F][{[color.name for color in ColorEnum]}]|(?:\x1B\[|\x9B)[0-?]*[ -/]*[@-~])"
+    ).sub("", str(result.stdout.strip(" ").strip("\n").lower()))
     assert result.exit_code == 0
-    assert "version table" in result.stdout.lower().strip("\n").strip(" ")
+    assert "version table" in output
+    assert typer.__version__ in output and __version__ in output
