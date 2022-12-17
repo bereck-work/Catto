c6e71aaf69a41c0e33fdaf1b9e4ac2bd9540ec16
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-12-17 23:54:16 +0530

    Updated the test for the help command and added more extensive changes.

--- tests/test_help.py
+++ tests/test_help.py
@@ -1,11 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import re
+
 from typer.testing import CliRunner
 
 from src.catto import app
+from src.catto.utils import ColorEnum
 
 runner = CliRunner()
 
 
-def test_app():
+def test_help_command():
     result = runner.invoke(app, ["--help"])
+    output = re.compile(
+        rf"(\x1B[@-Z\\-_]|[\x80-\x9A\x9C-\x9F][{[color.name for color in ColorEnum]}]|(?:\x1B\[|\x9B)[0-?]*[ -/]*[@-~])"
+    ).sub("", str(result.stdout.strip(" ").strip("\n").lower()))
+    print(output)
     assert result.exit_code == 0
-    assert "show this message and exit." in result.stdout.strip(" ").strip("\n").lower()
+    assert "show this message and exit." in output
+
+    for command in app.registered_commands:
+        if command.name.lower() in output and command.help.lower() in output:
+            assert True
+
+        else:
+            continue
